31bda823c0630419cd28591ffa1d1bf66796c57d
isaacwr <isaac.rothschild@gmail.com>
2015-10-18 14:24:30 -0400

    pushing to gh-pages

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,22 @@
-// Your JavaScript goes here...
+// Simple XMLHttpRequest usage and JSON parsing
+
+var blob = "";
+function saveData() {
+	blob = this.responseTest;
+}
+
+
+var xhr = new XMLHttpRequest();
+xhr.addEventListener("load", saveData);
+xhr.open("GET", "data.json");
+xhr.send();
+
+function parse() {
+	console.log(blob);	
+}
+console.log(blob);
+
+/*
+for each id in blob
+	add message and username to messages innerHTML
+*/
